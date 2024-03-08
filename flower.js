5ddca6f53e1f5f73b929666030ea9e8f08be0a9a
NamOfNames <ckn0203@gmail.com>
2024-03-08 10:06:23 +0700

    commit

---
+++ flower.js
@@ -0,0 +1,290 @@
+const red = document.getElementById("red");
+const pink = document.getElementById("pink");
+const white = document.getElementById("white");
+const yellow = document.getElementById("yellow");
+const orange = document.getElementById("orange");
+const blue = document.getElementById("blue");
+const flower1 = document.querySelector(".flower-1");
+const flower2 = document.querySelector(".flower-2");
+const flower3 = document.querySelector(".flower-3");
+
+red.addEventListener("click", () => {
+  flower1.childNodes[1].style.boxShadow = "0 0 10px #d1123f";
+  flower1.childNodes[3].style.boxShadow = "0 0 10px #d1123f";
+  flower1.childNodes[5].style.boxShadow = "0 0 10px #d1123f";
+  flower1.childNodes[7].style.boxShadow = "0 0 10px #d1123f";
+  flower1.childNodes[9].style.boxShadow = "0 0 10px #d1123f";
+
+  flower1.childNodes[1].style.background = "red";
+  flower1.childNodes[3].style.background = "red";
+  flower1.childNodes[5].style.background = "#cb0e40";
+  flower1.childNodes[7].style.background = "#cb0e40";
+  flower1.childNodes[9].style.background = "#b21c0e";
+
+  flower2.childNodes[1].style.boxShadow = "0 0 10px #d1123f";
+  flower2.childNodes[3].style.boxShadow = "0 0 10px #d1123f";
+  flower2.childNodes[5].style.boxShadow = "0 0 10px #d1123f";
+  flower2.childNodes[7].style.boxShadow = "0 0 10px #d1123f";
+  flower2.childNodes[9].style.boxShadow = "0 0 10px #d1123f";
+
+  flower2.childNodes[1].style.background = "red";
+  flower2.childNodes[3].style.background = "red";
+  flower2.childNodes[5].style.background = "#cb0e40";
+  flower2.childNodes[7].style.background = "#cb0e40";
+  flower2.childNodes[9].style.background = "#b21c0e";
+
+  flower3.childNodes[1].style.boxShadow = "0 0 10px #d1123f";
+  flower3.childNodes[3].style.boxShadow = "0 0 10px #d1123f";
+  flower3.childNodes[5].style.boxShadow = "0 0 10px #d1123f";
+  flower3.childNodes[7].style.boxShadow = "0 0 10px #d1123f";
+  flower3.childNodes[9].style.boxShadow = "0 0 10px #d1123f";
+
+  flower3.childNodes[1].style.background = "red";
+  flower3.childNodes[3].style.background = "red";
+  flower3.childNodes[5].style.background = "#cb0e40";
+  flower3.childNodes[7].style.background = "#cb0e40";
+  flower3.childNodes[9].style.background = "#b21c0e";
+});
+
+pink.addEventListener("click", () => {
+  flower1.childNodes[1].style.boxShadow = "0 0 10px #d1123f";
+  flower1.childNodes[3].style.boxShadow = "0 0 10px #d1123f";
+  flower1.childNodes[5].style.boxShadow = "0 0 10px #d1123f";
+  flower1.childNodes[7].style.boxShadow = "0 0 10px #d1123f";
+  flower1.childNodes[9].style.boxShadow = "0 0 10px #d1123f";
+
+  flower1.childNodes[1].style.background = "#fe7696";
+  flower1.childNodes[3].style.background = "#fe7696";
+  flower1.childNodes[5].style.background = "#f03a64";
+  flower1.childNodes[7].style.background = "#f03a64";
+  flower1.childNodes[9].style.background = "#d1123f";
+
+  flower2.childNodes[1].style.boxShadow = "0 0 10px #d1123f";
+  flower2.childNodes[3].style.boxShadow = "0 0 10px #d1123f";
+  flower2.childNodes[5].style.boxShadow = "0 0 10px #d1123f";
+  flower2.childNodes[7].style.boxShadow = "0 0 10px #d1123f";
+  flower2.childNodes[9].style.boxShadow = "0 0 10px #d1123f";
+
+  flower2.childNodes[1].style.background = "#fe7696";
+  flower2.childNodes[3].style.background = "#fe7696";
+  flower2.childNodes[5].style.background = "#f03a64";
+  flower2.childNodes[7].style.background = "#f03a64";
+  flower2.childNodes[9].style.background = "#d1123f";
+
+  flower3.childNodes[1].style.boxShadow = "0 0 10px #d1123f";
+  flower3.childNodes[3].style.boxShadow = "0 0 10px #d1123f";
+  flower3.childNodes[5].style.boxShadow = "0 0 10px #d1123f";
+  flower3.childNodes[7].style.boxShadow = "0 0 10px #d1123f";
+  flower3.childNodes[9].style.boxShadow = "0 0 10px #d1123f";
+
+  flower3.childNodes[1].style.background = "#fe7696";
+  flower3.childNodes[3].style.background = "#fe7696";
+  flower3.childNodes[5].style.background = "#f03a64";
+  flower3.childNodes[7].style.background = "#f03a64";
+  flower3.childNodes[9].style.background = "#d1123f";
+});
+
+white.addEventListener("click", () => {
+  flower1.childNodes[1].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower1.childNodes[3].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower1.childNodes[5].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower1.childNodes[7].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower1.childNodes[9].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+
+  flower1.childNodes[1].style.background = "#FAF8E8";
+  flower1.childNodes[3].style.background = "#FAF8E8";
+  flower1.childNodes[5].style.background = "#F0EBC5";
+  flower1.childNodes[7].style.background = "#F0EBC5";
+  flower1.childNodes[9].style.background = "#E3D4B1";
+
+  flower2.childNodes[1].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower2.childNodes[3].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower2.childNodes[5].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower2.childNodes[7].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower2.childNodes[9].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+
+  flower2.childNodes[1].style.background = "#FAF8E8";
+  flower2.childNodes[3].style.background = "#FAF8E8";
+  flower2.childNodes[5].style.background = "#F0EBC5";
+  flower2.childNodes[7].style.background = "#F0EBC5";
+  flower2.childNodes[9].style.background = "#E3D4B1";
+
+  flower3.childNodes[1].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower3.childNodes[3].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower3.childNodes[5].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower3.childNodes[7].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+  flower3.childNodes[9].style.boxShadow = "0 0 10px rgb(235, 235, 235)";
+
+  flower3.childNodes[1].style.background = "#FAF8E8";
+  flower3.childNodes[3].style.background = "#FAF8E8";
+  flower3.childNodes[5].style.background = "#F0EBC5";
+  flower3.childNodes[7].style.background = "#F0EBC5";
+  flower3.childNodes[9].style.background = "#E3D4B1";
+});
+
+yellow.addEventListener("click", () => {
+  flower1.childNodes[1].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower1.childNodes[3].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower1.childNodes[5].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower1.childNodes[7].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower1.childNodes[9].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+
+  flower1.childNodes[1].style.background = "#f3e557";
+  flower1.childNodes[3].style.background = "#f3e557";
+  flower1.childNodes[5].style.background = "#fbdd29";
+  flower1.childNodes[7].style.background = "#fbdd29";
+  flower1.childNodes[9].style.background = "#eeb702";
+
+  flower2.childNodes[1].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower2.childNodes[3].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower2.childNodes[5].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower2.childNodes[7].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower2.childNodes[9].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+
+  flower2.childNodes[1].style.background = "#f3e557";
+  flower2.childNodes[3].style.background = "#f3e557";
+  flower2.childNodes[5].style.background = "#fbdd29";
+  flower2.childNodes[7].style.background = "#fbdd29";
+  flower2.childNodes[9].style.background = "#eeb702";
+
+  flower3.childNodes[1].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower3.childNodes[3].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower3.childNodes[5].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower3.childNodes[7].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+  flower3.childNodes[9].style.boxShadow = "0 0 10px rgb(200, 200, 0)";
+
+  flower3.childNodes[1].style.background = "#f3e557";
+  flower3.childNodes[3].style.background = "#f3e557";
+  flower3.childNodes[5].style.background = "#fbdd29";
+  flower3.childNodes[7].style.background = "#fbdd29";
+  flower3.childNodes[9].style.background = "#eeb702";
+});
+
+orange.addEventListener("click", () => {
+  flower1.childNodes[1].style.boxShadow = "0 0 10px orange";
+  flower1.childNodes[3].style.boxShadow = "0 0 10px orange";
+  flower1.childNodes[5].style.boxShadow = "0 0 10px orange";
+  flower1.childNodes[7].style.boxShadow = "0 0 10px orange";
+  flower1.childNodes[9].style.boxShadow = "0 0 10px orange";
+
+  flower1.childNodes[1].style.background = "#fab45e";
+  flower1.childNodes[3].style.background = "#fab45e";
+  flower1.childNodes[5].style.background = "#ed8530";
+  flower1.childNodes[7].style.background = "#ed8530";
+  flower1.childNodes[9].style.background = "#e9630a";
+
+  flower2.childNodes[1].style.boxShadow = "0 0 10px orange";
+  flower2.childNodes[3].style.boxShadow = "0 0 10px orange";
+  flower2.childNodes[5].style.boxShadow = "0 0 10px orange";
+  flower2.childNodes[7].style.boxShadow = "0 0 10px orange";
+  flower2.childNodes[9].style.boxShadow = "0 0 10px orange";
+
+  flower2.childNodes[1].style.background = "#fab45e";
+  flower2.childNodes[3].style.background = "#fab45e";
+  flower2.childNodes[5].style.background = "#ed8530";
+  flower2.childNodes[7].style.background = "#ed8530";
+  flower2.childNodes[9].style.background = "#e9630a";
+
+  flower3.childNodes[1].style.boxShadow = "0 0 10px orange";
+  flower3.childNodes[3].style.boxShadow = "0 0 10px orange";
+  flower3.childNodes[5].style.boxShadow = "0 0 10px orange";
+  flower3.childNodes[7].style.boxShadow = "0 0 10px orange";
+  flower3.childNodes[9].style.boxShadow = "0 0 10px orange";
+
+  flower3.childNodes[1].style.background = "#fab45e";
+  flower3.childNodes[3].style.background = "#fab45e";
+  flower3.childNodes[5].style.background = "#ed8530";
+  flower3.childNodes[7].style.background = "#ed8530";
+  flower3.childNodes[9].style.background = "#e9630a";
+});
+
+blue.addEventListener("click", () => {
+  flower1.childNodes[1].style.boxShadow = "0 0 10px skyblue";
+  flower1.childNodes[3].style.boxShadow = "0 0 10px skyblue";
+  flower1.childNodes[5].style.boxShadow = "0 0 10px skyblue";
+  flower1.childNodes[7].style.boxShadow = "0 0 10px skyblue";
+  flower1.childNodes[9].style.boxShadow = "0 0 10px skyblue";
+
+  flower1.childNodes[1].style.background = "#d6f8f1";
+  flower1.childNodes[3].style.background = "#d6f8f1";
+  flower1.childNodes[5].style.background = "#99cae7";
+  flower1.childNodes[7].style.background = "#99cae7";
+  flower1.childNodes[9].style.background = "#5a6c9a";
+
+  flower2.childNodes[1].style.boxShadow = "0 0 10px skyblue";
+  flower2.childNodes[3].style.boxShadow = "0 0 10px skyblue";
+  flower2.childNodes[5].style.boxShadow = "0 0 10px skyblue";
+  flower2.childNodes[7].style.boxShadow = "0 0 10px skyblue";
+  flower2.childNodes[9].style.boxShadow = "0 0 10px skyblue";
+
+  flower2.childNodes[1].style.background = "#d6f8f1";
+  flower2.childNodes[3].style.background = "#d6f8f1";
+  flower2.childNodes[5].style.background = "#99cae7";
+  flower2.childNodes[7].style.background = "#99cae7";
+  flower2.childNodes[9].style.background = "#5a6c9a";
+
+  flower3.childNodes[1].style.boxShadow = "0 0 10px skyblue";
+  flower3.childNodes[3].style.boxShadow = "0 0 10px skyblue";
+  flower3.childNodes[5].style.boxShadow = "0 0 10px skyblue";
+  flower3.childNodes[7].style.boxShadow = "0 0 10px skyblue";
+  flower3.childNodes[9].style.boxShadow = "0 0 10px skyblue";
+
+  flower3.childNodes[1].style.background = "#d6f8f1";
+  flower3.childNodes[3].style.background = "#d6f8f1";
+  flower3.childNodes[5].style.background = "#99cae7";
+  flower3.childNodes[7].style.background = "#99cae7";
+  flower3.childNodes[9].style.background = "#5a6c9a";
+});
+
+const MakeFlower = () => {
+  for (let i = 4; i < 10; i++) {
+    const rose = document.createElement("div");
+    const flower = document.createElement("div");
+    const petal = document.createElement("div");
+    const petal1 = document.createElement("div");
+    const petal2 = document.createElement("div");
+    const petal3 = document.createElement("div");
+    const petal4 = document.createElement("div");
+
+    const leaf = document.createElement("div");
+    const stem = document.createElement("div");
+    const leafs = document.createElement("div");
+    const leafs1 = document.createElement("div");
+
+    rose.classList.add(`rose-${i}`);
+    flower.classList.add(`flower-${i}`);
+    petal.classList.add("petal");
+    petal1.classList.add("petal");
+    petal2.classList.add("petal");
+    petal3.classList.add("petal");
+    petal4.classList.add("petal");
+
+    leaf.classList.add("leaf");
+    stem.classList.add("stem");
+    leafs.classList.add("leafs");
+    leafs1.classList.add("leafs");
+
+    flower.appendChild(petal);
+    flower.appendChild(petal1);
+    flower.appendChild(petal2);
+    flower.appendChild(petal3);
+    flower.appendChild(petal4);
+
+    leaf.appendChild(stem);
+    leaf.appendChild(leafs);
+    leaf.appendChild(leafs1);
+
+    rose.appendChild(flower);
+    rose.appendChild(leaf);
+    rose.style.width = `calc(100px + ${i * 10})`;
+    rose.style.height = `calc(100px + ${i * 10}`;
+    rose.style.position = "absolute";
+    rose.style.right = "0";
+    rose.style.bottom = "0";
+    rose.style.display = "inline-block";
+    rose.style.animation = "yidon 5s infinite";
+    rose.style.transform = "translateY(40%)";
+    document.body.appendChild(rose);
+  }
+};
+
